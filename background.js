5aaa8bc2cde4a1e1a2d9889f1d1183eb9b5faa8b
BreakTos <76687690+BreakTos@users.noreply.github.com>
2023-04-08 14:37:32 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.contextMenus.create({
-    title: "Send text to my extension",
-    contexts: ["selection"],
-    onclick: function(info) {
-        var selectedText = info.selectionText;
-        selectedText="https://fakeornot.vercel.app/?search="+selectedText;
-        window.open(selectedText); 
-    }
-});
